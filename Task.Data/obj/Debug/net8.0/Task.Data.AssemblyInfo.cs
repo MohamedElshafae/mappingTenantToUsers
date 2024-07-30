437a20700046e1fda8d295e0b245207796bb2df1
Mohamed Elshfae <mohamedelshafae888@gmail.com>
2024-07-30 19:55:36 +0300

    final

--- Task.Data/obj/Debug/net8.0/Task.Data.AssemblyInfo.cs
+++ Task.Data/obj/Debug/net8.0/Task.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87f9f2604c0587acba0d96be187f6e704050d74e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
